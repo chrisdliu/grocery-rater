8ee24da2e299071ca20abafad85a3f2f98ebf17a
Christopher Liu <chrisliu@berkeley.edu>
2018-11-07 18:56:00 -0800

    Initial Commit

---
+++ Grocery Rater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocery Rater
+//
+//  Created by Chris Liu on 11/7/18.
+//  Copyright © 2018 cathris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
